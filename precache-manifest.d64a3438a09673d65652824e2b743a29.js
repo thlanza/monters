764412408642da583d80fd5e85523f80c9893ba3
Thiago Lanza <thlanza@gmail.com>
2019-10-07 14:16:15 -0300

    Updates

---
+++ precache-manifest.d64a3438a09673d65652824e2b743a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7859b60233df8a55ebb5a38e0a4ebd9e",
+    "url": "/monters/index.html"
+  },
+  {
+    "revision": "f88cd1c6cdf51731a625",
+    "url": "/monters/static/css/main.2215c443.chunk.css"
+  },
+  {
+    "revision": "ca474460bebc81d9af68",
+    "url": "/monters/static/js/2.64bf20a0.chunk.js"
+  },
+  {
+    "revision": "f88cd1c6cdf51731a625",
+    "url": "/monters/static/js/main.775db606.chunk.js"
+  },
+  {
+    "revision": "8855a8a85c06a758c5bf",
+    "url": "/monters/static/js/runtime-main.54cb9a1e.js"
+  }
+]);
